5f6f3a34234d797c9d2663bbf8092ca8d125fb86
Molham290 <Molhamwaleed77@gmail.com>
2026-07-05 11:32:54 +0300

    Adding context instruction and temperature for task1

--- Week1/FirstTask.py
+++ Week1/FirstTask.py
@@ -2,17 +2,23 @@ import os
 from google import genai
 from dotenv import load_dotenv
 
-
 load_dotenv()
-
 
 os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
 
-
 client = genai.Client()
+
 interaction = client.interactions.create(
     model="gemini-3.5-flash",
-    input="best use of ai."
+    input="what is the best model in ai.",
+    
+    
+    system_instruction="You are a senior tech expert. Provide very concise, practical answers.",
+    
+    
+    generation_config={
+        "temperature": 0.7
+    }
 )
 
 print(interaction.output_text)
