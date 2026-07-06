cafd15805e42e8a63a2b6afd8acf26a137868ce7
Molham290 <Molhamwaleed77@gmail.com>
2026-07-06 12:16:09 +0300

    Add Task 2: Prompt engineering comparison and Ollama chat interface

---
+++ Week1/Task2.py
@@ -0,0 +1,43 @@
+import ollama
+
+
+messages = [
+    {
+        'role': 'system'
+        
+    }
+]
+
+print("=== Context Window OPEN (Ollama). Type 'exit' to close ===")
+
+
+while True:
+    
+    user_input = input("\nAsk Q: ")
+    
+    
+    if user_input.lower() == 'exit':
+        print("Closing window...")
+        break
+
+    
+    messages.append({'role': 'user', 'content': user_input})
+
+    
+    response = ollama.chat(
+        model='llama3',
+        messages=messages,
+        options={
+            "temperature": 0.2,
+            "num_predict": 1000  
+        }
+    )
+
+   
+    ai_response = response['message']['content']
+    
+    
+    print("\n The A:", ai_response)
+
+    
+    messages.append({'role': 'assistant', 'content': ai_response})
